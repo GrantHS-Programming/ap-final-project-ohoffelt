0ba3c464e480ccb101e040b2fb7826ad0e3533d9
ohoffelt4013 <ohoffelt4013@student.pps.net>
2024-05-28 11:39:06 -0700

    may 28

--- Project.py
+++ Project.py
@@ -9,7 +9,7 @@ mainClock = pygame.time.Clock()
 SCALE = 3
 
 WINDOWWIDTH = 700 * SCALE
-WINDOWHEIGHT = 300*SCALE
+WINDOWHEIGHT = 300 * SCALE
 window = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Jesus Jumps')
 
@@ -17,16 +17,15 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (175, 0, 0)
-DARK_RED = (150,0,0)
-IDK = (100, 15, 15)
-BLUE = (0, 100, 255)
+DARK_RED = (75, 30, 30)
+REDDISH = (100, 15, 15)
+BLUE = (0, 150, 225)
 DARK_BLUE = (0, 75, 200)
 BROWN = (150, 100, 50)
-SKY = (0, 200, 255)
+LIGHT_BLUE = (0, 200, 255)
 YELLOW = (255, 255, 0)
 GRAY = (100, 100, 100)
 DARK_GREY = (75, 75, 75)
-
 
 charW = 20 * SCALE
 charH = 35 * SCALE
@@ -41,13 +40,30 @@ jesus = pygame.image.load("jesus.png")
 wave = pygame.image.load("wave.png")
 crate = pygame.image.load("crate.png")
 cross = pygame.image.load("cross.png")
+fish1 = pygame.image.load("fish1.png")
+fish1 = pygame.transform.flip(fish1,True,False)
+fish2 = pygame.image.load("fish2.png")
+fish3 = pygame.image.load("fish3.png")
+fish4 = pygame.image.load("fish4.png")
+fish5 = pygame.image.load("fish5.png")
+fish6 = pygame.image.load("fish6.png")
+fish7 = pygame.image.load("fish7.png")
+fish8 = pygame.image.load("fish8.png")
+fish = [fish1, fish2, fish3, fish4, fish5, fish6, fish7, fish8]
+rock = pygame.image.load("not_netherrack.jpeg")
+y = 0
+for x in fish:
+    x = pygame.transform.scale(x, (blockD, blockD))
+    fish[y] = x
+    y += 1
 jesus = pygame.transform.scale(jesus, (charW, charW))
-wave = pygame.transform.scale(wave,(blockD,blockD))
-crate = pygame.transform.scale(crate,(blockD,blockD))
-cross = pygame.transform.scale(cross, (charW,charH))
+wave = pygame.transform.scale(wave, (blockD, blockD))
+crate = pygame.transform.scale(crate, (blockD, blockD))
+cross = pygame.transform.scale(cross, (charW, charH))
 cross = pygame.transform.rotate(cross, 40)
-jesus_upside_down = pygame.transform.flip(jesus,False, True)
+jesus_upside_down = pygame.transform.flip(jesus, False, True)
 jesus_rotated = pygame.transform.rotate(jesus, -90)
+rock = pygame.transform.scale(rock, (blockD, blockD))
 
 char = pygame.Rect(0 + charW * 3, WINDOWHEIGHT / 2 - charH / 2, charW, charW)
 terrain = pygame.Rect(0, WINDOWHEIGHT - terrainH, WINDOWWIDTH, terrainH)
@@ -84,6 +100,7 @@ def createDirtLevel():
 
 
 def createWaterLevel():
+    print(len(fish))
     block_x = []
     block_y = []
     blocks.clear()
@@ -97,7 +114,8 @@ def createWaterLevel():
     for x in block_x:
         for y in range(30):
             block = {"rect": pygame.Rect(WINDOWWIDTH + random.randint(x - xVariance, x + xVariance),
-                                         random.randint(0, WINDOWHEIGHT - blockD), blockD, blockD), "color": DARK_BLUE}
+                                         random.randint(0, WINDOWHEIGHT - blockD), blockD, blockD),
+                     "fish": fish[random.randint(0, len(fish) - 1)]}
             add = True
             for i in block_y:
                 if block["rect"].colliderect(i):
@@ -175,7 +193,8 @@ def dirtLevel():
                         speed_up = 0
 
             for i in blocks:
-                if char.colliderect(i["rect"]) and char.bottom > i["rect"].top + 25 * SCALE and char.right <= i["rect"].left + 10 * SCALE:
+                if char.colliderect(i["rect"]) and char.bottom > i["rect"].top + 25 * SCALE and char.right <= i[
+                    "rect"].left + 10 * SCALE:
                     playing = False
                 if ((char.colliderect(i["rect"]) and char.bottom <= i["rect"].top + 12 * SCALE) or (
                         char.colliderect(i["rect"]) and grav >= 10 * SCALE)) and grav >= up:
@@ -209,7 +228,7 @@ def dirtLevel():
             window.blit(score, score_rect)
             window.blit(high_score, high_score_rect)
             pygame.display.update()
-            window.fill(SKY)
+            window.fill(LIGHT_BLUE)
             mainClock.tick(40)
     return high_score_temp
 
@@ -222,18 +241,18 @@ def waterLevel():
         up = False
         down = False
         vert = 0
-        game_speed = 4 * SCALE
+        game_speed = 2 * SCALE
         frame_count = 0
         jump_count = 0
         speed_up = 0
         score_counter = 0
-        grace_period = 50
+        grace_period = 100
         createWaterLevel()
         char.centery = window.get_rect().centery
 
         while playing:
-            if game_speed <= 10:
-                game_speed += 0.001 * SCALE
+            if game_speed <= 15:
+                game_speed += 0.0005 * SCALE
             score = basicFont.render(str(int(score_counter) * 10), True, WHITE, None)
             high_score = basicFontSmall.render(str(int(high_score_temp) * 10), True, WHITE, None)
             score_rect = score.get_rect()
@@ -256,9 +275,11 @@ def waterLevel():
                         exit = True
                         playing = False
                     elif (event.key == K_d or event.key == K_RIGHT) and frame_count >= grace_period:
-                        speed_up = 2 * SCALE
+                        speed_up = 1 * SCALE
                     elif (event.key == K_a or event.key == K_LEFT) and frame_count >= grace_period:
-                        speed_up = -2 * SCALE
+                        speed_up = -1 * SCALE
+                    elif event.key == K_0:
+                        char.top = WINDOWHEIGHT
                 if event.type == KEYUP:
                     if event.key == K_UP or event.key == K_w:
                         up = False
@@ -278,12 +299,12 @@ def waterLevel():
                     high_score_temp = score_counter
                 if i["rect"].right <= 0:
                     i["rect"].right += levelSize
-                window.blit(wave, i["rect"])
+                window.blit(i["fish"], i["rect"])
 
             if up:
-                vert -= 0.5 * SCALE
+                vert -= 0.2 * SCALE
             if down:
-                vert += 0.5 * SCALE
+                vert += 0.2 * SCALE
             if not (up or down):
                 vert /= 1.05
 
@@ -292,13 +313,14 @@ def waterLevel():
             if char.bottom < 0:
                 char.bottom = WINDOWHEIGHT
 
-            if frame_count > grace_period or frame_count % 20 < 10:
+            if frame_count > grace_period or frame_count % 40 < 20:
                 window.blit(jesus_rotated, char)
             window.blit(score, score_rect)
             window.blit(high_score, high_score_rect)
-            pygame.display.update()
-            window.fill(SKY)
-            mainClock.tick(40)
+            if frame_count % 2 == 0:
+                pygame.display.update()
+            window.fill(BLUE)
+            mainClock.tick(80)
     return high_score_temp
 
 
@@ -359,7 +381,7 @@ def hellLevel():
                 grav_dir *= -1
                 char.centery += grav_dir * 2 * SCALE
             for i in blocks:
-                i["rect"].centerx -= game_speed+speed_up
+                i["rect"].centerx -= game_speed + speed_up
                 if i["rect"].right < 0:
                     i["rect"].centerx += levelSize
                 if i["rect"].colliderect(char):
@@ -373,7 +395,7 @@ def hellLevel():
                             char.top = i["rect"].bottom - 1
                     else:
                         playing = False
-                pygame.draw.rect(window, RED, i["rect"])
+                window.blit(rock, i["rect"])
             if collided:
                 on_ground = True
                 grav = 0
@@ -397,14 +419,14 @@ def hellLevel():
             pygame.draw.rect(window, DARK_RED, terrain)
             pygame.draw.rect(window, DARK_RED, ceiling)
             if frame_count <= grace_period and frame_count % 20 >= 10:
-                pygame.draw.rect(window, IDK, char)
+                pygame.draw.rect(window, REDDISH, char)
             score_counter += (game_speed + speed_up) / (6 * SCALE)
             if high_score_temp < score_counter:
                 high_score_temp = score_counter
             window.blit(score, score_rect)
             window.blit(high_score, high_score_rect)
             pygame.display.update()
-            window.fill(IDK)
+            window.fill(REDDISH)
             mainClock.tick(40)
     return high_score_temp
 
@@ -421,7 +443,6 @@ def checkOnGround(i, grav_dir):
 
 
 while True:
-
     highScoreSpace = hellLevel()
     highScoreCloud = waterLevel()
     highScoreDirt = dirtLevel()
